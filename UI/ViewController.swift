610f80b7f7ed433368ef90a8ee189d1d51f74d42
TanyEm <tanyem@protonmail.com>
2020-07-01 12:49:55 +0300

    Initial Commit

---
+++ UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI
+//
+//  Created by Tatiana Podlesnykh on 1.7.2020.
+//  Copyright © 2020 Tatiana Podlesnykh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

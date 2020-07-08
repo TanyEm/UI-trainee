9c8c82ca6e23b9f3117beb93167b4635a5b79132
TanyEm <tanyem@protonmail.com>
2020-07-09 00:53:42 +0300

    Add left boarder

--- UI/PhoneNumberViewController.swift
+++ UI/PhoneNumberViewController.swift
@@ -24,34 +24,24 @@ class PhoneNumberViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        regionNumber.layer.cornerRadius = 15
+        regionNumber.layer.cornerRadius = 20
         regionNumber.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
         regionNumber.layer.borderWidth = 0
         regionNumber.clipsToBounds = true
-        
-        let bottomBorder = CALayer()
-        let width = CGFloat(1.0)
-        bottomBorder.frame = CGRect(x: 0.0, y: regionNumber.frame.size.height - width, width: regionNumber.frame.size.width, height: width)
-        bottomBorder.backgroundColor = UIColor.black.cgColor
-        regionNumber.layer.addSublayer(bottomBorder)
-        
-        let topBorder = CALayer()
-        topBorder.frame = CGRect(x: 0.0, y: 0.0, width: regionNumber.frame.size.width, height: width)
-        topBorder.backgroundColor = UIColor.black.cgColor
-        regionNumber.layer.addSublayer(topBorder)
-        
+                
         let rightBorder = CALayer()
-        rightBorder.frame = CGRect(x: regionNumber.frame.size.width - width, y: 0.0, width: width, height: regionNumber.frame.size.height - width)
-        rightBorder.backgroundColor = UIColor.black.cgColor
-        regionNumber.layer.addSublayer(rightBorder)
+        let width = CGFloat(1.0)
+        rightBorder.frame = CGRect(x: 0.0, y: 0.0, width: width, height: phoneNumber.frame.size.height - width)
+        rightBorder.backgroundColor = UIColor.gray.cgColor
+        phoneNumber.layer.addSublayer(rightBorder)
         
         
-        phoneNumber.layer.cornerRadius = 15
+        phoneNumber.layer.cornerRadius = 20
         phoneNumber.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
         phoneNumber.layer.borderWidth = 0
         phoneNumber.clipsToBounds = true
         
-        moveNext.layer.cornerRadius = 15
+        moveNext.layer.cornerRadius = 20
                 
     }
     

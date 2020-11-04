fb73a94f5518ff43cd6645715736c6532a9eee7b
TanyEm <tanyem@protonmail.com>
2020-11-04 16:05:22 +0200

    Change a button font, background opacity and color; Clear code

--- UI/PhotoEditorViewController.swift
+++ UI/PhotoEditorViewController.swift
@@ -128,14 +128,23 @@ class PhotoEditorViewController: UIViewController, UIImagePickerControllerDelega
         //after the image is rendered on UI get the real dimensions
         //Size of the Image that is rendered on UI in proper aspect ratio
         let renderedImageSize: CGSize = CGSize(width: 400, height: 400)
-
+        let xFrameParameter = Double(renderedImageSize.width/originalImageSize.width)*buttonsInfo.x
+        let yFramexParameter = Double(renderedImageSize.height/originalImageSize.height)*buttonsInfo.y
+        
         let button = UIButton()
         button.tag = buttonsInfo.id
-        button.frame = CGRect(x:Double(renderedImageSize.width/originalImageSize.width)*buttonsInfo.x,
-                               y: Double(renderedImageSize.height/originalImageSize.height)*buttonsInfo.y,
+        button.frame = CGRect(x: xFrameParameter,
+                              y: yFramexParameter,
                               width: 70,
                               height: 30)
-        button.backgroundColor = UIColor.lightGray
+        button.backgroundColor = UIColor(red: 200.0/255.0,
+                                         green: 200.0/255.0,
+                                         blue: 200.0/255.0,
+                                         alpha:0.5)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "SF Compact Rounded Bold", size: 17.0)
+        button.layer.cornerRadius = 10
+        
         button.setTitle("Edit", for: .normal)
         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
 
